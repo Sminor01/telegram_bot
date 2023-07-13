12f18bcec0ef75f0c2115ae516d466864f9ee31e
Alexsandr <82975768+Sminor01@users.noreply.github.com>
2023-07-13 16:56:12 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require 'vendor/autoload.php';
 
 use WeStacks\TeleBot\TeleBot;
 
-$bot = new TeleBot('5203607843:AAF2W1nIsas5R2waSQD0LOyeIrFX2GFBKK0'); // API-токен бота здесь
+$bot = new TeleBot('#'); // API-токен бота здесь
 $bot_user = $bot->getMe();
 
 var_dump($bot_user);
